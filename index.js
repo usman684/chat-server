d33dd9df3d56fb4275d09a4e1e955b0d342ce9df
Muhammad Usman <usman.rauf.953@gmail.com>
2026-04-20 22:39:05 +0500

    link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 
 // 🔥 IMPORTANT: Replace frontend URL after deploy
-const FRONTEND_URL = "https://your-frontend.vercel.app";
+const FRONTEND_URL = "https://chat-server-production-aea7.up.railway.app/";
 
 const io = new Server(server, {
   cors: {
